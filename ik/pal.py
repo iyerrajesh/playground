8d96f539079f4b93eea4bb6bf6ebc37c64773e1d
Rajesh Chandrasekaran <rajeshchandrasekaran@rajeshs-mbp-2.corp.balbix.com>
2020-02-28 17:29:46 -0800

    add olap

--- ik/pal.py
+++ ik/pal.py
@@ -23,18 +23,55 @@ def expand(s, st, e):
 	return out
 
 
-def pal_susbtr(s):
+def expand_max(s, st, e):
+	l = len(s)
+	s_max = (-1,-1)
+	while st >= 0 and e < l and s[st] == s[e]:
+		s_max = (st, e)
+		st -= 1
+		e += 1
+	return s_max
+
+
+def pal_substr(s):
 	ln = len(s)
-	num = 0
+	l_pal = 0
+	l_pal_str = ''
 
 	for i in range(ln):
-		odd = expand(s,i,i)
-		even = expand(s,i,i+1)
-		num += len(odd) + len(even)
+		x,y = expand_max(s,i,i)
+		if l_pal < y-x+1:
+			l_pal = y-x+1
+			l_pal_str = s[x:y+1]
+		x, y = expand_max(s,i,i+1)
+		if l_pal < y-x+1:
+			l_pal = y-x+1
+			l_pal_str = s[x:y+1]
+	return l_pal_str
 
-		#print(odd, even)
 
-	return num
+def is_overlap(rec1, rec2):
+	recs = [rec1, rec2]
+	#recs.sort(key=lambda x:x[0])
+
+	rec1, rec2 = recs
+	if rec2[0] == rec1[0]:
+		if rec2[1] < rec1[1]:
+			rec1, rec2 = rec2, rec1
+
+	if rec2[0] < rec1[2]:
+		if rec2[1] < rec1[3]:
+			return True
+
+	if rec2[1] < rec1[3]:
+		if rec2[0] < rec1[2]:
+			return True
+
+	return False
 
 
-print (pal_susbtr('bbcc'))
+#print (pal_substr("babadaadfudgigbhhbgrt"))
+r1 = [-2,1,0,3]
+r2 = [-2,0,0,1]
+
+print (is_overlap(r1,r2))

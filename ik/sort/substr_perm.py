c26437efb38ef687e1503fa93a91b73132fd31b1
Rajesh Chandrasekaran <iyer.rajesh1@gmail.com>
2020-03-16 15:32:06 -0700

    add min sliding win

--- ik/sort/substr_perm.py
+++ ik/sort/substr_perm.py
@@ -40,3 +40,55 @@ def str_perm(s1, s2):
 s1 = 'fools'
 s2 = 'heyfurlolsof'
 print str_perm(s1,s2)
+
+def min_array_w_nums(A, nums):
+	l = len(A)
+	n_hash = defaultdict(lambda:0)
+	idxs = []
+	x  = 0
+	m_s, m_e = 0, 0 
+	m_min = sys.maxint 
+
+	i = j = 0
+
+	while j < l:
+		while j < l:
+			if len(n_hash) < len(nums):
+				if A[j] in nums:
+					n_hash[A[j]] += 1
+					idxs.append(j)
+			if len(n_hash) == len(nums):
+				i = idxs[x]
+				if j-i+1 < m_min:
+					m_s, m_e = i,j
+					m_min = j-i+1
+				break
+			j += 1
+
+		while len(n_hash) == len(nums):
+			if j-i+1 < m_min:
+				m_s, m_e = i,j
+				m_min = j-i+1
+
+			n_hash[A[i]] -= 1
+			if n_hash[A[i]] == 0:
+				del (n_hash[A[i]])
+
+			x += 1
+			i = idxs[x]
+			
+		j += 1
+
+	return m_s, m_e
+
+
+s = {1,3,2}
+#A = [1,2,2,3,-4,0,1,1,2,2,0,3,3,2,2,0,1]
+A = [4,5,6,7,9,1,2,4,5,6]
+A = [1,2,5,8,7,6,2,6,5,3,8,5]
+s = s = {5,7}
+#print (min_array_w_nums(A,s))
+
+A = 'ANCSNBAXC'
+s = 'CN'
+print (min_array_w_nums(A, s))
